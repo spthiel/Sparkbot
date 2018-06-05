3590e3f7900a0b7b8d4c63c1eb755a09256781eb
spthiel <steppi.thiel@googlemail.com>
2018-06-05 12:47:58 +0200

    Update Constants.java

--- Sparkbot/src/main/java/me/main/Constants.java
+++ Sparkbot/src/main/java/me/main/Constants.java
@@ -3,7 +3,7 @@ package me.main;
 public class Constants {
     
     protected static final String
-        TOKEN = "Mzk4MDQwMTk0OTk1MDYwNzM2.DS4vhA.YtNnvFD2HW5MihTKRu-334g66W4"
+        TOKEN = "TOKEN"
             
             ;
 
